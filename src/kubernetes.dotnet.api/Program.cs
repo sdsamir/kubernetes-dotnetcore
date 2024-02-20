7edf521e69d7cf04d1b780700cb9ad5ba19df683
Samir Kumar Dutta <sdsamirkumardutta@gmail.com>
2024-02-20 17:52:35 +0530

    Api Default Path Added

--- src/kubernetes.dotnet.api/Program.cs
+++ src/kubernetes.dotnet.api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/", () => "Welcome to Kubernetes Api Home Page || Running Ok");
 
 app.UseHttpsRedirection();
 
